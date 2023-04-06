5fbd057f3808847d92e7c753839b3eab35e4dc97
HiepNM <hiepnm@vmodev.com>
2023-04-07 01:30:30 +0700

    update modules

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+import classes from "./home.module.scss";
+
+const HomePage: FC = () => {
+  return <div className={classes.Container}>Home</div>;
+};
+
+export default HomePage;
