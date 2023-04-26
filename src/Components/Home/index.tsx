6d9e2966c9f65dd66a9ff899a6efff8f18155ea2
Vion Nguyen <hiepnm@vmodev.com>
2023-04-26 14:01:58 +0700

    update banner home

--- src/Components/Home/index.tsx
+++ src/Components/Home/index.tsx
@@ -1,12 +1,18 @@
 import { FC } from "react";
 import classes from "./home.module.scss";
+import HomeBanner from "../../assets/img/PA2.png";
 
 const HomePage: FC = () => {
-  return <div className={classes.Container}>
-    <div id="content" className={classes.siteContent}>
-      <div className={classes.Container}>Home</div>
+  return (
+    <div className={classes.Container}>
+      <div
+        className={classes.sectionBannerImg}
+        style={{ backgroundImage: `url(${HomeBanner.src})` }}
+      >
+        <div className={classes.setContent}></div>
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default HomePage;
