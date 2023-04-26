3daf5f875b1e9dd5cb2ada665b8bfb91019b7dca
Vion Nguyen <hiepnm@vmodev.com>
2023-04-26 18:45:01 +0700

    update content banner

--- src/Components/Home/index.tsx
+++ src/Components/Home/index.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import classes from "./home.module.scss";
 import HomeBanner from "../../assets/img/PA2.png";
+import Link from "next/link";
 
 const HomePage: FC = () => {
   return (
@@ -9,7 +10,35 @@ const HomePage: FC = () => {
         className={classes.sectionBannerImg}
         style={{ backgroundImage: `url(${HomeBanner.src})` }}
       >
-        <div className={classes.setContent}></div>
+        <div className={classes.overWriteBg}></div>
+        <div className={classes.setContent}>
+          <div className={classes.rowContent}>
+            <div className={classes.content}>
+              <div className={classes.itemTop}>
+                <div className={classes.coverItemTop}>
+                  <h2 className={classes.titleTop}>Let’s Create Your…</h2>
+                </div>
+              </div>
+              <div className={classes.itemSec}>
+                <div className={classes.coverItemSec}>
+                  <h1 className={classes.titleSec}>Own Style with K.A.R.A.!</h1>
+                </div>
+              </div>
+              <div className={classes.itemBottom}>
+                <div className={classes.wrapItem}>
+                  <div className={classes.itemLeft}>
+                    <div className={classes.coverItemLeft}>
+                      <div className={classes.wrapItemLeft}>
+                        <Link href={'#'} ><span>Read More</span></Link>
+                      </div>
+                    </div>
+                  </div>
+                  <div className={classes.itemRight}>Watch Video</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
