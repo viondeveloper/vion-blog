353bda9910cd2ea100ff1c42c40facc77f2e6750
Vion Nguyen <hiepnm@vmodev.com>
2023-04-28 17:50:35 +0700

    update meta tag

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import HomePage from '../Components/Home'
+import HomePage from "../Components/Home";
 import { NextPage } from "next";
 
 const Home: NextPage = () => {
@@ -7,8 +7,15 @@ const Home: NextPage = () => {
     <>
       <Head>
         <title>Vion Nguyen - Blog and About Me</title>
-        <meta name="description" content="Hi everybody, I am Nguyen Minh Hiep - Vion Nguyen" />
-        <meta name="og:description" content="Hi everybody, I am Nguyen Minh Hiep - Vion Nguyen. Programming is my life and I am a Front-end Developer" />
+        <meta
+          name="description"
+          content="Hi everybody, I am Nguyen Minh Hiep - Vion Nguyen"
+        />
+        <meta property="title" content="Welcome, I’m glad you’re here" />
+        <meta
+          property="og:description"
+          content="Hi everybody, I am Nguyen Minh Hiep - Vion Nguyen. Programming is my life and I am a Front-end Developer"
+        />
         <meta name="author" content="Nguyen Minh Hiep" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
